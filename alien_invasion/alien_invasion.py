094255bd7eb17a64f025d91910e501b488ac80e6
yuyang <yangshufe@163.com>
2018-05-09 21:04:55 +0800

    Import Ship class to make a ship.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize pygame, settings and screen object.
@@ -9,8 +10,13 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    # Set the background color
+
+    # Make a ship.
+    ship = Ship(screen)
+
+    # Set the background color.
     bg_color = (230, 230, 230)
+
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
@@ -19,6 +25,7 @@ def run_game():
                 sys.exit()
             # 每次循环时都重绘屏幕
             screen.fill(ai_settings.bg_color)
+            ship.blitme()
             # 让最近绘制的屏幕可见
             pygame.display.flip()
 
