bc85f845b29fbf3f562e971055416755497782d4
yuyang <yangshufe@163.com>
2018-05-10 18:19:33 +0800

    Write fire_bullets() function to reconstruct the check_keydown_events() function.

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -11,10 +11,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a bullet and add it into the bullets group.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullets(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
@@ -23,15 +20,23 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_setting, screen, ship, bullets):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse clicks."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_setting, screen, ship, bullets)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+
+def fire_bullets(ai_settings, screen, ship, bullets):
+    """If not yet reach the limit, fire a bullet."""
+    # Create a bullet and add it into the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
 
 def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new one."""
