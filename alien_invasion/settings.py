1c1e5ba754a661c7bc20cd0ff9751e4d3155560c
yuyang <yangshufe@163.com>
2018-05-10 18:06:46 +0800

    Set maximum bullets allowed.

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -13,7 +13,8 @@ class Settings():
         # Bullet settings.
         self.bullet_speed_factor = 1
         self.bullet_width = 3
-        self.bullet_hight = 15
+        self.bullet_height = 15
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
 
 
