c2bf464dc09e479180c83799555c65eda535cefd
yuyang <yangshufe@163.com>
2018-05-10 15:12:24 +0800

    Use movement flags to control the movement of ships.

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -14,7 +14,16 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Movement flags.
+        self.moving_right = False
+
+    def update(self):
+        """Adjust the ship's position, based on movement flags."""
+        if self.moving_right:
+            self.rect.centerx += 1
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
 
