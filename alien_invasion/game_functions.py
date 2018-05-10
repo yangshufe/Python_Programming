eef830769ff60d70921e4c4f84107222436ab3a4
yuyang <yangshufe@163.com>
2018-05-10 16:54:23 +0800

    Add the bullet feature.

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -1,13 +1,19 @@
 import sys
 import pygame
 
-def check_keydown_events(event, ship):
+from bullet import Bullet
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
         # Move the ship to the right.
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create a bullet and add it into the bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
@@ -16,20 +22,24 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(ai_setting, screen, ship, bullets):
     """Respond to keypresses and mouse clicks."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_setting, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new one."""
     # Redraw the screen for each pass through the loop.
     screen.fill(ai_settings.bg_color)
+
+    # Redraw all bullets behind ships and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
 
     ship.blitme()
 
